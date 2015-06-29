bc48d02decd012e3fd59b19a7309dbeff2484cbc
lspir <leandrospir@gmail.com>
2015-06-29 18:26:02 -0300

    pequeño fix pattern matching

--- src/Evolucion.scala
+++ src/Evolucion.scala
@@ -2,7 +2,7 @@ case class Evolucion(especie:Especie,condicion:CondicionEvolucion) {
   
   def cumpleCondicionNivel(pokemon:Pokemon):Boolean={
     condicion match{
-      case subirDeNivel => condicion.teCumple(pokemon)
+      case _:subirDeNivel => condicion.teCumple(pokemon)
       case _ => false
     }
   } 
