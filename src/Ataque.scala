2073096e60c073fed3edf83296c4fab5b2fa5270
Cristian Patricio Contreras <Pato94_06@hotmail.com>
2015-06-30 00:15:41 -0300

    humilde aporte nocturno

--- src/Ataque.scala
+++ src/Ataque.scala
@@ -1,5 +1,7 @@
+import javax.swing.text.html.Option
+
 case class Ataque(tipo:Tipo,maximoInicial:Int,efecto:Option[(Pokemon=>Pokemon)]) {
-  def aplicarEfecto(pokemon:Pokemon):Pokemon={
+  def aplicarEfecto(pokemon:Try[Pokemon]):Try[Pokemon]={
     efecto.map(f=>f(pokemon)).getOrElse(pokemon)
     }
 }
--- src/Pokemon.scala
+++ src/Pokemon.scala
@@ -1,7 +1,9 @@
+import java.util
+
 case class Pokemon(experiencia:Int, genero:Genero,energia:Int,energiaMaximaOriginal:Int,pesoOriginal:Int,
-    fuerzaOriginal:Int,velocidadOriginal:Int,estado:Estado,especie:Especie,ataques:List[Ataque]) {
+    fuerzaOriginal:Int,velocidadOriginal:Int,estado:Estado,especie:Especie,ataques:util.List[Ataque]) {
   
-  type Actividad=Object=>Pokemon;
+  type Actividad=Any=>Try[Pokemon];
   
   def nivel:Int=especie.obtenerNivel(experiencia);
   def energiaMaxima=especie.aumentar(energiaMaximaOriginal,nivel);
@@ -25,14 +27,35 @@ case class Pokemon(experiencia:Int, genero:Genero,energia:Int,energiaMaximaOrigi
   
   
   val realizarUnAtaque:Actividad={
-      case ataque:Ataque => ataque.aplicarEfecto( this.subirExperiencia(ataque.tipo match{
+      case ataque:Ataque => ataques.filter(ataque == _).aplicarEfecto( this.subirExperiencia(ataque.tipo match{
         case Dragon => 80
         case this.especie.tipoPrincipal => 50
         case this.especie.tipoSecundario => this.genero match{
           case Macho => 20
           case Hembra => 40
         }
-      }))
+      })).getOrElse(new Exception);
   }
-  
+
+  val realizar(actividad:Actividad, elem:Object):Try[Pokemon]={
+    this.estado match{
+      case e:Dormido => copy(estado = e.dormir)
+      case KO => new Exception()
+      case _ => this.actividad(elem)
+    }
+  }
+
+  val levanatarPesas(unosKilos:Int):Try[Pokemon]={
+    this.estado match{
+      case Paralizado => copy(estado = KO)
+      case _ => if (unosKilos>10*tfuerza && this.especie.tipoPrincipal != Fantasma) {
+        copy(estado = Paralizado)
+      } else (this.especie.tipoPrincipal, this.especie.tipoSecundario) match{
+        case (Fantasma, _) => new Exception()
+        case (Pelea, _) => this.subirExperiencia(2*unosKilos)
+        case (_, Some(Pelea)) => this.subirExperiencia(2*unosKilos)
+        case _ => this.subirExperiencia(unosKilos)
+      }
+    }
+  }
 }
